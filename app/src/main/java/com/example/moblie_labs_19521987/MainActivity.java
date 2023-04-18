788332dc27da37a496c62c4ee8f79b38520fb3b2
unknown <vanphappi3.14@gmail.com>
2023-04-18 21:48:04 +0700

    submit for lab2

--- app/src/main/java/com/example/moblie_labs_19521987/MainActivity.java
+++ app/src/main/java/com/example/moblie_labs_19521987/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.moblie_labs_19521987;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText inputFullName;
+    private EditText inputGrossSalary;
+    private TextView outputInformation;
+    private Button buttonCalculate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonCalculate= findViewById(R.id.buttonCalculate);
+        outputInformation = findViewById(R.id.outputInformation);
+        buttonCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputFullName = findViewById(R.id.inputFullName);
+                inputGrossSalary = findViewById(R.id.inputGrossSalary);
+
+                String fullName;
+                fullName = inputFullName.getText().toString();
+                int grossSalary;
+                try {
+                    grossSalary = Integer.parseInt(inputGrossSalary.getText().toString());
+                }
+                catch(Exception e) {
+                    grossSalary = 0;
+                }
+                PersonalSalary preson = new PersonalSalary(fullName,grossSalary);
+                outputInformation.setText(outputInformation.getText()+"\n"+preson.getInformation());
+                inputFullName.setText(null);
+                inputGrossSalary.setText(null);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/moblie_labs_19521987/PersonalSalary.java
@@ -0,0 +1,29 @@
+package com.example.moblie_labs_19521987;
+
+import java.math.BigDecimal;
+
+public class PersonalSalary {
+    private String fullName;
+    private long grossSalary;
+    private final long dependentCost  = 11000000;
+
+    PersonalSalary(String fullName, int grossSalary){
+        this.fullName = fullName;
+        this.grossSalary = grossSalary;
+    }
+
+    public double netSalary(){
+        double netSalary=0;
+        double temp = grossSalary*(1-0.105);
+        if(temp <= dependentCost)
+            return temp;
+        netSalary = dependentCost + (temp - dependentCost)*(1-0.05);
+        return netSalary;
+    }
+
+    public String getInformation(){
+        if(this.fullName == "" && this.grossSalary == 0 )
+            return "";
+        return fullName + " - Net Salary: " + new BigDecimal(netSalary()).toPlainString();
+    }
+}
