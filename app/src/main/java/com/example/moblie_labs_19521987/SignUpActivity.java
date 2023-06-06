fd347069c58a691eb666f0fb9b4dbd4c98ba1a7c
vanpuyn <vanphappi3.14@gmail.com>
2023-06-06 23:13:23 +0700

    up

--- app/src/main/java/com/example/moblie_labs_19521987/SignUpActivity.java
+++ app/src/main/java/com/example/moblie_labs_19521987/SignUpActivity.java
@@ -1,7 +1,10 @@
 package com.example.moblie_labs_19521987;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,14 +16,19 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
@@ -35,11 +43,15 @@ public class SignUpActivity extends AppCompatActivity {
     Button signup;
 
     TextView backtologin;
-    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private FirebaseAuth mAuth;
+     private FirebaseFirestore firestore;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_page);
+        mAuth = FirebaseAuth.getInstance();
+
+        firestore = FirebaseFirestore.getInstance();
 
         AppCompatActivity activity = (AppCompatActivity) SignUpActivity.this;
         if (activity != null && activity.getSupportActionBar() != null) {
@@ -69,7 +81,6 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(isValidUsername(user.getText().toString()) && pass.getText().toString().length() >= 6){
-
                     String PassEncrypted = encodeString(pass.getText().toString());
 
                     String UserName = user.getText().toString();
@@ -77,35 +88,62 @@ public class SignUpActivity extends AppCompatActivity {
                     String Phone = phone.getText().toString();
 
                     String FullName = fullname.getText().toString();
-                    firebaseAuth.createUserWithEmailAndPassword(UserName,PassEncrypted).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            FirebaseUser user = firebaseAuth.getCurrentUser();
-                            String userUid = user.getUid();
 
-                            HashMap<Object, String> users= new HashMap<>();
-                            users.put("username", UserName);
-                            users.put("password", PassEncrypted);
-                            users.put("ID", userUid);
-                            users.put("fullname", FullName);
-                            users.put("phone", Phone);
 
-                            FirebaseDatabase db = FirebaseDatabase.getInstance();
-                            DatabaseReference ref = db.getReference("user");
-                            ref.child(userUid).setValue(users);
+                    Map<String, Object> user = new HashMap<>();
+                    user.put("username", UserName);
+                    user.put("password", PassEncrypted);
+                    user.put("fullname", FullName);
+                    user.put("phone", Phone);
 
-                            Toast.makeText(SignUpActivity.this, "Sign up successfully!", Toast.LENGTH_LONG).show();
+                    firestore.collection("users")
+                            .add(user)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    Log.d("DEBUG", "DocumentSnapshot added with ID: " + documentReference.getId());
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w("DEBUG", "Error adding document", e);
+                                }
+                            });
 
-                            Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
-                            startActivity(intent);
-                            finish();
-                        }
-                    });
 
-                }else{
+                    Toast.makeText(SignUpActivity.this, "Sign up successfully!", Toast.LENGTH_LONG).show();
+
+                    Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else{
+
                     Toast.makeText(SignUpActivity.this, "username contains at least 6 alphanumeric characters, password at least 6 characters !", Toast.LENGTH_SHORT).show();
                     return;
 
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    private void login(String gmail,String pass){
+        mAuth.createUserWithEmailAndPassword(gmail,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Log.d("DEBUG", "Login sucessfull");
+                }
+                else {
+                    Log.d("DEBUG", "Login fail");
                 }
             }
         });
